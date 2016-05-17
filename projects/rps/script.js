5a9ee743c5993df714ffeed071d2b252a4e36027
Brian Lee <blee025432@gmail.com>
2016-05-17 10:12:39 -0400

    tried to implement some javascript
    not too successfull

--- projects/rps/script.js
+++ projects/rps/script.js
@@ -1,1 +1,23 @@
-confirm("Does this show?");
+var rock = function() {
+  var userChoice = "rock";
+};
+
+var scissors = function() {
+  var userChoice = "scissors";
+};
+
+var paper = function() {
+  var userChoice = "paper";
+};
+
+if (userChoice == "rock") {
+  confirm("The user picked rock.");
+}
+
+else if (userChoice == "paper") {
+  confirm("the user picked paper");
+}
+
+else if (userChoice == "scissors"){
+  confirm("the user picked paper");
+};
