78d605b233722ad8c96baff9f04b8c58805893df
Brian Lee <blee025432@gmail.com>
2016-11-11 23:05:26 -0500

    fixing damn hover

---
+++ test4/hover.js
@@ -0,0 +1,16 @@
+function maintrigger(){
+  console.log("I got here");
+  var thing = document.getElementById("main");
+  linehighlight(thing);
+}
+
+
+function linehighlight(block){
+
+  block.style.backgroundColor = "blue";
+}
+
+
+function blockhighlight(){
+
+}
