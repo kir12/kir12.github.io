7d57f594353e73a531576498e941fc4005714b6e
Brian Lee <blee025432@gmail.com>
2016-12-26 23:33:38 -0500

    finsihed song module

--- crystal/audio.js
+++ crystal/audio.js
@@ -3,7 +3,14 @@ function audioLoad(){
     "snowdin.mp3",
     "snowy.mp3",
     "grinch.mp3",
-    "joy_world.mp3"
+    "joy_world.mp3",
+    "song5.mp3",
+    "faithful.mp3",
+    "night.mp3",
+    "santa.mp3",
+    "rockaround.mp3",
+    "deckthehalls.mp3"
+
   ];
     var randomIndex = Math.floor(Math.random() * songs.length);
     var song = songs[randomIndex];
